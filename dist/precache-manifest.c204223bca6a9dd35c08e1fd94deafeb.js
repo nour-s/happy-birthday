993fac185789ff4aa9ba82b5420ad17e175673ab
Nour Sabouny <programmer.nour@gmail.com>
2021-12-11 01:57:00 +0400

    push dist for Github pages

---
+++ dist/precache-manifest.c204223bca6a9dd35c08e1fd94deafeb.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14dac9f89669e792b03",
+    "url": "game.85c5b2b9d34279f22531.js"
+  },
+  {
+    "revision": "cfaefed83226aceb68e8",
+    "url": "vendors.feee7ddb057a807f8dd0.js"
+  },
+  {
+    "revision": "9c19efece75068ba7e1b3b821bea8533",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "89459e19dfccb74f48899f878f252408",
+    "url": "assets/backgrounds/clouds_MG_1_lightened.png"
+  },
+  {
+    "revision": "e55a8c3c7e9d2943a33feeb484b4e1bf",
+    "url": "assets/backgrounds/cloud_lonely.png"
+  },
+  {
+    "revision": "8483c2c3896ff8db5048261fc66e169b",
+    "url": "assets/backgrounds/clouds_BG.png"
+  },
+  {
+    "revision": "59a4e4378c407f1521e4f131cf8236bc",
+    "url": "assets/backgrounds/clouds_MG_1.png"
+  },
+  {
+    "revision": "b70e82be1998cd7156bfc5ac580fb0bf",
+    "url": "assets/backgrounds/clouds_MG_3.png"
+  },
+  {
+    "revision": "6720e51f519cf129903d74f7869b35a0",
+    "url": "assets/backgrounds/clouds_MG_2.png"
+  },
+  {
+    "revision": "beb79e0fba09d06d6f8c764c8e447233",
+    "url": "assets/backgrounds/mountains_lightened.png"
+  },
+  {
+    "revision": "949e15e96f13a4a0945d05982690a1e2",
+    "url": "assets/fonts/arcade.ttf"
+  },
+  {
+    "revision": "78112502adf5443224059311cdef86ba",
+    "url": "assets/backgrounds/mountains.png"
+  },
+  {
+    "revision": "fbb5a642c01d22443d43ef6f62fac9a2",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "d7bc86b430abbee54a71bf49b7e7bdce",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "1db0dd741c9a8672080fbd920f4bdc1e",
+    "url": "assets/images/button.png"
+  },
+  {
+    "revision": "4b33b18f79aab29e30314404e9124458",
+    "url": "assets/images/dialog.png"
+  },
+  {
+    "revision": "47a22af0104ee3c17678f8d9cb249011",
+    "url": "assets/sprites/door.png"
+  },
+  {
+    "revision": "d684da2114fe3f1df12d1bbd63753f42",
+    "url": "assets/sprites/heart.png"
+  },
+  {
+    "revision": "2123a11c0fc4e4aad3f892a96a09eae6",
+    "url": "assets/sprites/empty.png"
+  },
+  {
+    "revision": "f35199e325ded5b220bb8514f5e32f18",
+    "url": "assets/sprites/orly.png"
+  },
+  {
+    "revision": "cad5a792c2b6e39f4eae83a31c9e6e56",
+    "url": "assets/tilemaps/atlas.png"
+  },
+  {
+    "revision": "554ab99d699ac203e2b21ae4d9948a66",
+    "url": "assets/sprites/love_chest.png"
+  },
+  {
+    "revision": "8dc2a33095a73b4de389d85f399866a7",
+    "url": "assets/tilemaps/base.tmx"
+  },
+  {
+    "revision": "76bfc10024f10500aa745a9a05eacb42",
+    "url": "assets/tilemaps/atlas.tsx"
+  },
+  {
+    "revision": "4e63728bbc83b550cea0a89e64e974a8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "3914903e49b054a324b74be5e7d8a964",
+    "url": "assets/tilemaps/base.json"
+  },
+  {
+    "revision": "7dff1540d2af72a0002e55db059a77e3",
+    "url": "assets/tilemaps/extruded.png"
+  }
+]);
