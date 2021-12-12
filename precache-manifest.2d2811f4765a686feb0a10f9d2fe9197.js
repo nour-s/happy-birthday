c587972684d9b5e16e995197ad62680bbaa01c6e
Github Pages Overwriter <GithubPagesOverwriter@users.noreply.github.com>
2021-12-12 07:38:12 +0000

    Automated publish

--- precache-manifest.2d2811f4765a686feb0a10f9d2fe9197.js
+++ precache-manifest.2d2811f4765a686feb0a10f9d2fe9197.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e14dac9f89669e792b03",
-    "url": "game.85c5b2b9d34279f22531.js"
+    "revision": "5a2f677cbf22acb0c295",
+    "url": "game.15d37db7657ac4c11ee2.js"
   },
   {
     "revision": "cfaefed83226aceb68e8",
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "favicon.ico"
   },
   {
-    "revision": "89459e19dfccb74f48899f878f252408",
-    "url": "assets/backgrounds/clouds_MG_1_lightened.png"
+    "revision": "969fd39b2504d55e8d690868e2efa141",
+    "url": "manifest.json"
   },
   {
     "revision": "e55a8c3c7e9d2943a33feeb484b4e1bf",
@@ -28,24 +28,28 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/backgrounds/clouds_MG_1.png"
   },
   {
-    "revision": "b70e82be1998cd7156bfc5ac580fb0bf",
-    "url": "assets/backgrounds/clouds_MG_3.png"
+    "revision": "89459e19dfccb74f48899f878f252408",
+    "url": "assets/backgrounds/clouds_MG_1_lightened.png"
   },
   {
     "revision": "6720e51f519cf129903d74f7869b35a0",
     "url": "assets/backgrounds/clouds_MG_2.png"
   },
   {
+    "revision": "b70e82be1998cd7156bfc5ac580fb0bf",
+    "url": "assets/backgrounds/clouds_MG_3.png"
+  },
+  {
     "revision": "beb79e0fba09d06d6f8c764c8e447233",
     "url": "assets/backgrounds/mountains_lightened.png"
   },
   {
-    "revision": "949e15e96f13a4a0945d05982690a1e2",
-    "url": "assets/fonts/arcade.ttf"
-  },
-  {
     "revision": "78112502adf5443224059311cdef86ba",
     "url": "assets/backgrounds/mountains.png"
+  },
+  {
+    "revision": "949e15e96f13a4a0945d05982690a1e2",
+    "url": "assets/fonts/arcade.ttf"
   },
   {
     "revision": "fbb5a642c01d22443d43ef6f62fac9a2",
@@ -68,15 +72,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/sprites/door.png"
   },
   {
-    "revision": "d684da2114fe3f1df12d1bbd63753f42",
-    "url": "assets/sprites/heart.png"
-  },
-  {
     "revision": "2123a11c0fc4e4aad3f892a96a09eae6",
     "url": "assets/sprites/empty.png"
   },
   {
-    "revision": "f35199e325ded5b220bb8514f5e32f18",
+    "revision": "d684da2114fe3f1df12d1bbd63753f42",
+    "url": "assets/sprites/heart.png"
+  },
+  {
+    "revision": "554ab99d699ac203e2b21ae4d9948a66",
+    "url": "assets/sprites/love_chest.png"
+  },
+  {
+    "revision": "2c7015dd6151d4fe423f5fc8048efe55",
     "url": "assets/sprites/orly.png"
   },
   {
@@ -84,24 +92,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/tilemaps/atlas.png"
   },
   {
-    "revision": "554ab99d699ac203e2b21ae4d9948a66",
-    "url": "assets/sprites/love_chest.png"
-  },
-  {
-    "revision": "8dc2a33095a73b4de389d85f399866a7",
-    "url": "assets/tilemaps/base.tmx"
-  },
-  {
-    "revision": "76bfc10024f10500aa745a9a05eacb42",
+    "revision": "39dee448de074a97f6d49cac82cba6a6",
     "url": "assets/tilemaps/atlas.tsx"
   },
   {
-    "revision": "4e63728bbc83b550cea0a89e64e974a8",
-    "url": "manifest.json"
+    "revision": "1de27531f32f81fadb42b4bdfaee8af9",
+    "url": "assets/tilemaps/base.json"
   },
   {
-    "revision": "3914903e49b054a324b74be5e7d8a964",
-    "url": "assets/tilemaps/base.json"
+    "revision": "656c95aa0cf6db7469272cfd87111d62",
+    "url": "assets/tilemaps/base.tmx"
   },
   {
     "revision": "7dff1540d2af72a0002e55db059a77e3",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.c204223bca6a9dd35c08e1fd94deafeb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.2d2811f4765a686feb0a10f9d2fe9197.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(__precacheManifest);
 
