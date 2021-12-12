0014c6a40563c1fdfecc002481cc2d0fa6cb9581
Nour Sabouny <programmer.nour@gmail.com>
2021-12-12 11:37:07 +0400

    update CNAME to include www

---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.c204223bca6a9dd35c08e1fd94deafeb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest);
+
